a96bf7aebfde144c1dfc9bc8270d0f90adec03cf
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-09 15:34:46 +0900

    ユーザー登録、認証画面のスタイリング設定。複数ユーザーの登録確認

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignUp />;
+    return <div className="flex justify-center items-center h-screen bg-gray-200">
+                <SignUp />
+            </div>
 }
