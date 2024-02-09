7b60af3e6c51b909aad2875751dafb2dd0801b16
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-09 13:29:46 +0900

    環境構築実施、ディレクトリ構造作成、必要モジュールインストールした。

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
+
+const noto_sans = Noto_Sans_JP({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
+export const metadata: Metadata = {
+  title: "Events Booking System",
+  description: "Organize your own event!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={noto_sans.className}>{children}</body>
+    </html>
+  );
+}
