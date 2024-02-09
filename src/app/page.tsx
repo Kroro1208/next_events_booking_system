ea99304f8e96d7f2c46f3e3e6bdd1a52c3bde8d6
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-09 16:30:20 +0900

    スキーマ設定してユーザー情報を出力して確認した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { connectDB } from "../config/dbConfig";
 import { UserButton } from "@clerk/nextjs";
+// import { auth, currentUser } from "@clerk/nextjs/server";
 connectDB();
 
-export default function Home() {
+export default async function Home() {
+  // const { userId } = auth();
+  // const user = await currentUser();
+  // console.log(user?.username);
+  // console.log(userId);
   return (
     <div className="p-10">
       <UserButton afterSignOutUrl="/" />
