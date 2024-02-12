7bf83bc388081fda9e7217ed0f423b85d8bb67de
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-12 20:57:46 +0900

    firebaseに画像をアップロードできるようにした。

--- src/app/admin/events/_cpmponents/event-form/Tickets.tsx
+++ src/app/admin/events/_cpmponents/event-form/Tickets.tsx
@@ -8,7 +8,7 @@ function Tickets({ event, setEvent, activeStep, setActiveStep }: EventFormStepPr
     const onAddTicketType = () => {
         try {
             const tempEvent = { ...event };
-            if (event.ticketTypes) { // チケットのタイプで枚数制限を加える（例えばプレミアは稀少にする）
+            if (tempEvent.ticketTypes) { // eventオブジェクトにticketTypesがあるならticketTypesオブジェクトに格納
                 tempEvent.ticketTypes.push({
                     name: "",
                     price: 0,
@@ -16,13 +16,14 @@ function Tickets({ event, setEvent, activeStep, setActiveStep }: EventFormStepPr
                     limit: 0
                 });
             } else {
-                tempEvent.ticketTypes = [{ // ノーマルチケットは枚数制限なし
+                tempEvent.ticketTypes = [{ // eventオブジェクトにticketTypesがなければticketTypesオブジェクトを作成
                     name: "",
                     price: 0,
+                    quantity: 0,
                     limit: 0
                 }];
             }
-            setEvent(tempEvent);
+            setEvent(tempEvent); // ここでもともとのeventオブジェクトを更新
         } catch (error: any) {
             toast.error(error.message);
         };
@@ -50,16 +51,16 @@ function Tickets({ event, setEvent, activeStep, setActiveStep }: EventFormStepPr
             {event.ticketTypes && event.ticketTypes.length > 0 && (
                 <div className='flex flex-col gap-5'>
                     <div className=' grid grid-cols-4 rounded justify-between p-2'>
-                        {["Name", "Price", "Limit", ""].map((item, index) => (
+                        {["チケット名及び種類", "価格 /円", "枚数制限", ""].map((item, index) => (
                             <h1 key={index} className='font-semibold'>{item}</h1>
                         ))}
                     </div>
                     {event.ticketTypes.map((ticketTypes: any, index: number) => (
                         <div className="grid grid-cols-4 font-semibold gap-5 items-center" key={index}>
-                            <Input placeholder='チケットの種類(Premieなど)' onChange={(e) => onTicketPropertyChange({ index, property: "name", value: e.target.value })} value={ticketTypes.name} />
-                            <Input placeholder='1枚当たりの価格' onChange={(e) => onTicketPropertyChange({ index, property: "price", value: e.target.value })} value={ticketTypes.price} />
-                            <Input placeholder='枚数制限' onChange={(e) => onTicketPropertyChange({ index, property: "limit", value: e.target.value })} value={ticketTypes.limit} />
-                            <Button className='bg-red-700 hover:bg-orange-500 rounded-full text-white w-28' onClick={() => onTicketTypeDelete(index)}><i className="ri-delete-bin-6-line"></i>削除する</Button>
+                            <Input placeholder='チケット名や種類(Premieなど)' onChange={(e) => onTicketPropertyChange({ index, property: "name", value: e.target.value })} value={ticketTypes.name} />
+                            <Input placeholder='1枚当たりの価格' onChange={(e) => onTicketPropertyChange({ index, property: "price", value: Number(e.target.value) })} value={ticketTypes.price} />
+                            <Input placeholder='枚数制限' onChange={(e) => onTicketPropertyChange({ index, property: "limit", value: Number(e.target.value) })} value={ticketTypes.limit} type='number' />
+                            <Button className='bg-red-600 hover:bg-red-400 rounded-full text-white w-28' onClick={() => onTicketTypeDelete(index)}><i className="ri-delete-bin-6-line"></i>削除する</Button>
                         </div>
                     ))}
                 </div>
@@ -70,8 +71,7 @@ function Tickets({ event, setEvent, activeStep, setActiveStep }: EventFormStepPr
                 onClick={onAddTicketType}><i className="ri-coupon-3-line"></i>チケットの種類を追加する</Button>
             <div className="flex justify-center gap-5">
                 <Button onClick={() => { setActiveStep(activeStep - 1) }}>戻る</Button>
-                <Button onClick={() => setActiveStep(activeStep + 1)} color='primary'
-                >次の入力画面へ</Button>
+                <Button type='submit' color='primary' isDisabled={event?.ticketTypes?.length === 0}>登録する</Button>
             </div>
         </div>
     )
--- src/app/admin/events/_cpmponents/event-form/index.tsx
+++ src/app/admin/events/_cpmponents/event-form/index.tsx
@@ -6,13 +6,25 @@ import General from './General'
 import LocationAndDate from './LocationAndDate'
 import Media from './Media'
 import Tickets from './Tickets'
+import { uploadImageToFirebaseAndGetUrls } from '@/src/helpers/image-upload'
+import toast from 'react-hot-toast'
 
 function EventForm() {
     const [activeStep = 0, setActiveStep] = useState<number>(0);
     const [newlySelectedImages = [], setNewlySelectedImages] = useState<any>([]);
     const [event, setEvent] = useState<any>(null);
+
+    const handleFormSubmit = async () => {
+        try {
+            event.images = await uploadImageToFirebaseAndGetUrls(newlySelectedImages.map((image: any) => image.file));
+            console.log(event);
+        } catch (error: any) {
+            toast.error(error.message);
+        }
+    }
     const onSubmit = (e: any) => {
         e.preventDefault();
+        handleFormSubmit();
     };
 
     const commonProps = {
