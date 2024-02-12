5bb6a1152091e819f4d617bc421fc7c14327cbeb
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-12 16:54:55 +0900

    ゲージバーの設定がこんがらがったのでコメント追加

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -15,19 +15,19 @@ function Steps({ stepNames, stepsContent, activeStep }: StepsProps) {
                         <div
                             className={`flex flex-col gap-2 ${index !== stepNames.length - 1 && "w-full"}`}>
                             <div className="flex items-center">
-                                <div
+                                <div // 数字の表示部分
                                     className={`h-8 w-8 rounded-full  flex justify-center items-center
-                                    ${activeStep >= index
+                                    ${activeStep >= index // 最初はどちらも0
                                             ? "bg-blue-600 text-white"
                                             : "bg-gray-500 text-gray-200"
                                         }`}>
                                     {index + 1}
                                 </div>
-
-                                {activeStep > index && index !== stepNames.length - 1 && (
+                                {/* ゲージバー表示部分: Steps.tsx, General.tsx, event-form/index.tsxを確認 */} 
+                                {activeStep > index && index !== stepNames.length - 1 && ( // setActive()によりactiveStepが更新された時
                                     <div className="h-1 w-full bg-blue-600"></div>
                                 )}
-                                {activeStep <= index && index !== stepNames.length - 1 && (
+                                {activeStep <= index && index !== stepNames.length - 1 && (// 次へを押さないとactiveStepは増えていかないから、現在のページにいる段階では以降のページはゲージが白くなるように設定
                                     <div className="h-1 w-full bg-gray-200"></div>
                                 )}
                             </div>
