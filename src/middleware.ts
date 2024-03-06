d7c8010bccfdcd44756e8f0d103a496f0329818b
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-07 08:17:13 +0900

    ミドルウェア修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/api/current-user"],
 });
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
