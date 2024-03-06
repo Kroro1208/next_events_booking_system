130d2466cb422e0084f30aa57aaf2e0589cc30c6
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-07 07:35:39 +0900

    ミドルウェア修正

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    productionBrowserSourceMaps: true,
     images: {
         domains: ["localhost", "firebasestorage.googleapis.com"]
     }
